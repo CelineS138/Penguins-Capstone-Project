518907b22e6df17287097afcb80a710360c45fa6
CelineS138 <c.yang.sue@outlook.com>
2024-06-26 15:43:23 -0400

    EDA & Metrics draft

---
+++ Celine Su/EDA & Examples.R
@@ -0,0 +1,5 @@
+
+# Load data set from Hockey Reference 
+
+Stats_AD23.24 <- read.csv("~/Desktop/Penguins Capstone Project/Stats_AD23-24.csv")
+Stats_AD22.23 <- read.csv("~/Desktop/Penguins Capstone Project/Stats_AD22-23.csv")
